a9bd76446afcd8012981d0044f66f8022344495e
PavelAC <pavelandreicalin@gmail.com>
2025-02-06 12:45:52 +0200

    loader and error handling

--- angular-template/src/app/auth/auth.component.ts
+++ angular-template/src/app/auth/auth.component.ts
@@ -2,15 +2,19 @@ import { Component } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms'; 
 import { AuthService } from "./auth.service";
+import { loadeingSpinnerComponent } from "../../../public/loading-spinner/loading-spinner.component";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html',
     standalone: true,
-      imports: [CommonModule, FormsModule],
+      imports: [CommonModule, FormsModule, loadeingSpinnerComponent],
 })
 export class AuthComponent{
     IsLogged = true;
+    IsLoading = false;
+    error: string = '';
+    
 
     constructor(private authService: AuthService) {}
 
@@ -22,15 +26,20 @@ export class AuthComponent{
         if(!form.valid){return;}
         const email =form.value.email;
         const password =form.value.password;
-
+        this.IsLoading =true;
         if(this.IsLogged){
 
         }else{
             this.authService.signup(email,password).subscribe(resData =>
                 {
                     console.log(resData);
+                    this.IsLoading =false;
                 },
-                error => {console.log(error)}
+                errorMessage => {
+                    console.log(errorMessage);
+                    this.error = errorMessage;
+                    this.IsLoading =false;
+                }
                 );
         } 
         form.reset();
